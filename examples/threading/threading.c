502c8e7f5cc1da94cf36c5920f5bea4a885d3674
Suyash Chandra <suyash.iitr@gmail.com>
2025-05-14 21:01:51 +0530

    Submission for assignment4 part1

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -13,10 +13,22 @@ void* threadfunc(void* thread_param)
 
     // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
     // hint: use a cast like the one below to obtain thread arguments from your parameter
-    //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
-    return thread_param;
+    struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    usleep(thread_func_args->wait_to_obtain_ms*1000);
+    int rc = pthread_mutex_lock(thread_func_args->mutex);
+    if (rc!=0){
+        ERROR_LOG("pthread mutex lock failed with %d\n", rc);
+    }else{
+        usleep(thread_func_args->wait_to_release_ms*1000);
+        rc = pthread_mutex_unlock(thread_func_args->mutex);
+        if (rc!=0){
+            ERROR_LOG("pthread mutex unlock failed with %d\n", rc);
+            thread_func_args->thread_complete_success = false;
+        }
+        thread_func_args->thread_complete_success = true;
+    }
+    return thread_func_args;
 }
-
 
 bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int wait_to_obtain_ms, int wait_to_release_ms)
 {
@@ -28,6 +40,17 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
      *
      * See implementation details in threading.h file comment block
      */
-    return false;
+    struct thread_data* thread_params = (struct thread_data *)malloc(sizeof(struct thread_data));
+    thread_params->wait_to_obtain_ms = wait_to_obtain_ms;
+    thread_params->wait_to_release_ms = wait_to_release_ms;
+    thread_params->mutex = mutex;
+    int rc = pthread_create(thread, NULL, threadfunc, thread_params);
+    if(rc!=0){
+        ERROR_LOG("Failed to create thread");
+        thread_params->thread_complete_success=false;
+    }else{
+        thread_params->thread_complete_success=true;
+    }
+    return thread_params->thread_complete_success;
 }
 
